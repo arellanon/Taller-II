0d01177947e2e6ff228c3db9e12ea2e9df1f06a4
nahuel <nahuel.arellano@gmail.com>
2017-04-26 19:20:05 -0300

    Se elimino la carpeta borrar

--- TPs/borrar/TP2_correciones/Ejercicio_8/example.py
+++
@@ -1,9 +0,0 @@
-from ConfigParser import SafeConfigParser
-
-parser = SafeConfigParser()
-parser.read('simple.ini')
-
-print parser.get('nodo1', 'host')
-print parser.get('nodo1', 'port')
-print parser.get('nodo2', 'host')
-print parser.get('nodo2', 'port')
--- TPs/borrar/_Ejercicio_7/ejer7.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from lib.ServerProxy import ServerProxy
-
-s = ServerProxy()
-s.iniciar_server()
